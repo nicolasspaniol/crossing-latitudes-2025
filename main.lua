537e9e6465bbd56942c4cb99efa6380840c35340
JDomingueti <jean.domingueti@gmail.com>
2025-05-31 16:13:20 -0300

    Adicionando interação com classe take-point

--- main.lua
+++ main.lua
@@ -1,13 +1,19 @@
 local CollageSource = require "collage_source"
-
+local takePoints = require "takePoints"
 
 --- @type CollageSource
 local colSrc = nil
 
+--- @type takePoints
+local tkp = nil
+
+
 function love.load()
   love.window.setMode(1280, 720)
-  colSrc = CollageSource.new(love.graphics.newImage("imagem.png"))
+  local img = love.graphics.newImage("imagem.png")
+  colSrc = CollageSource.new(img)
   colSrc:setTransform(love.math.newTransform(10, 10, 0, .3, .3))
+  tkp = takePoints.new(0, 0, img:getWidth(), img:getHeight())
 end
 
 
@@ -19,11 +25,13 @@ end
 function love.draw()
   colSrc:draw()
   colSrc:debugPoints()
+  tkp:draw()
 end
 
 
 function love.mousepressed(x, y, key)
   colSrc:mousepressed(x, y, key)
+  tkp:mousepressed(x, y, key, colSrc)
 
   local maybeSliceCnv = colSrc:getSlice()
 end
@@ -31,4 +39,9 @@ end
 
 function love.mousemoved(x, y)
   colSrc:mousemoved(x, y)
+end
+
+
+function love.keypressed(key)
+  tkp:keypressed(key)
 end
