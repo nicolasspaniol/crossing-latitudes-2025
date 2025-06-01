6419f02a43caccf4488fc99b31e44fa3f5bf16e8
JDomingueti <jean.domingueti@gmail.com>
2025-06-01 01:53:03 -0300

    arrumando

--- document.lua
+++ document.lua
@@ -1,35 +1,36 @@
 local Buttons = require "buttons"
+
+--- @class Document
 local Document = {}
+Document.__index = Document
+
+
 Document.isMoving = false
 
-Document.create = function(x, y, width, height, image)
-  
-  
-  local bttFunction = function(self, collages, selCollIdx, dType)
+Document.new = function(x, y, width, height, image, dType)
+  local bttFunction = function(self, collages, selCollIdx)
     if collages[selCollIdx] then
       table.insert(Document.collages, collages[selCollIdx])
     else
       Document.isMoving = not Document.isMoving
     end
   end
-  
+
   Document.type = dType
   Document.collages = {}
   Document.canva = love.graphics.newCanvas(width, height)
   Document.recorded_pos = { x = 0, y = 0 }
   Document.button = Buttons:new(image, x, y, width, height, bttFunction)
-
+  Document.dx = 0
+  Document.dy = 0
+  
   Document.button.draw = function(self, inCanvas, xCanvas, yCanvas)
     self.drawUpdate = false
     local sM = 1
-    if self.isHovered then
-        sM = 1
-    end
     if (self.image.type or "") == "animation" then
         self.drawUpdate = true
         local _, _, qw, qh = self.image.quads[1]:getViewport()
         local sx, sy = self.size.width/qw, self.size.height/qh
-        
         local x, y = (xCanvas or 0) + (1-sM)/2 * self.size.width, (yCanvas or 0) + (1-sM)/2 * self.size.height
         if not inCanvas then x, y = self.coords.x + (1-sM)/2 * self.size.width, self.coords.y + (1-sM)/2 * self.size.height end
         self.image:draw(x, y, 0, sM*sx, sM*sy)
@@ -40,13 +41,55 @@ Document.create = function(x, y, width, height, image)
         love.graphics.draw(self.image, x, y , 0, sM*self.size.width/iw, sM*self.size.height/ih) 
     end
   end
+
+  return Document
 end
+
 
 Document.update = function(mx, my, dt)
   Document.button:update(mx, my, dt)
-  --
 end
 
+
 Document.draw = function()
-  Document.button:draw(true, unpack(Document.button.coords) )
+  local xm, ym = love.mouse.getPosition()
+  if Document.isMoving then
+    Document.button.coords.x = xm - Document.dx
+    Document.button.coords.y = ym - Document.dy
+  end
+  Document.button:draw(true, Document.button.coords.x, Document.button.coords.y)
+  if #Document.collages > 0 then
+    for _, collage in Document.collages do
+      if Document.isMoving then
+        collage.transform:translate(xm, ym)
+      end
+      collage.draw()
+    end
+  end
 end
+
+
+Document.mousepressed = function(x, y, key, collages, selCollIdx)
+
+  if key == 1 then
+    local xp = Document.button.coords.x
+    local yp = Document.button.coords.y
+    if Document.isMoving then
+      Document.isMoving = false
+    elseif (x > xp and x < xp + Document.canva:getWidth()) and (y > yp and y < yp + Document.canva:getHeight()) then
+      Document.bttFunction(Document, collages, selCollIdx)
+      Document.dx = x - xp
+      Document.dy = y - yp
+      if #Document.collages > 0 then
+        for _, collage in ipairs(Document.collages) do
+          local xc, yc = collage.transform:transformPoint(0,0)
+          collage.dx = x - xc
+          collage.dy = y - yc
+        end
+      end
+    end
+  end
+end
+
+
+return Document
--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 local json = require "json/json"
 local MoveSource = require "move_source"
 local CollageSource = require "collage_source"
+local Document = require "document"
 
 --- @type CollageSource
 local colSrc = nil
@@ -8,6 +9,8 @@ local colSrc = nil
 --- @type MoveSource
 local mvSrc = nil
 
+--- @type Document
+local doc = nil
 
 function love.load()
   love.window.setMode(1280, 720)
@@ -16,26 +19,23 @@ function love.load()
   local points = json.decode(f:read("*a"))
   f:close()
   
+  doc = Document.new(800, 200, 200, 100, love.graphics.newImage("data/impres.png"))
   colSrc = CollageSource.new(love.graphics.newImage("imagem.png"), points)
-  colSrc:setTransform(love.math.newTransform(10, 10, 0, .3, .3))
+  colSrc:setTransform(love.math.newTransform(10, 10, 0, .1, .1))
   collages = {}
   currentCollage = 0
 end
 
 
 function love.update(dt)
-  if #collages > 0 then
-    for i, collage in ipairs(collages) do
-      if collage.pressed then currentCollage = i; break end
-    end
-  else
-    currentCollage = 0
-  end
+  local mx, my = love.mouse.getPosition()
+  doc.update(mx, my, dt)
 end
 
 
 function love.draw()
   colSrc:draw()
+  doc:draw()
   if #collages > 0 then
     for _, collage in ipairs(collages) do
       collage:draw()
@@ -46,6 +46,7 @@ end
 
 function love.mousepressed(x, y, key)
   local onCol = false
+  doc:mousepressed(x, y, key, collages, currentCollage)
   if #collages > 0 then
     if collages[currentCollage] and collages[currentCollage].pressed then
       collages[currentCollage]:mousepressed(x, y, key)
