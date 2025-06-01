c6445df70d44817bfcbb1f7d5a5332b1993f9707
Sílvio E. S. Zacomelli <zacomellisilvio@gmail.com>
2025-06-01 03:20:24 -0300

    created

--- drawer.lua
+++ drawer.lua
@@ -5,84 +5,94 @@ local Stamps = require "stamps"
 local Drawer = {}
 
 
-Drawer.createStamps = function(bx, by, bw, bh, images, openImage, closeImage, UIImage, stampWidthRatio, stampHeightRatio)
+Drawer.createEntities = function(bx, by, bw, bh, images, openImage, closeImage, UIImage, stampWidthRatio, stampHeightRatio)
   Drawer.coords = { x = bx, y = by }
   Drawer.size = { width = bw, height = bh }
 
-  local stampSize = math.min(math.ceil(bh*0.9/2), bw*0.9/2)
-  local stx1, sty1 = bx + bw*0.05, by + bh*0.05
+  local stampSize = math.min(math.ceil(bh*0.4/2), bw*0.5/2)
+  local stx1, sty1 = bx + bw*0.225, by + bh*0.1
   
   Drawer.stamps = {}
   for i = 1, #images do
-    table.insert(Drawer.stamps, Stamps:new(stx1 + bw*((i+1) % 2)/2, sty1 + stampSize*(i-1), stampSize, stampSize, 
+    table.insert(Drawer.stamps, Stamps:new(stx1 + 0.8*((i-1) %2)*(bx + bw)/4, sty1 + 0.5*stampSize*(i-1), stampSize, stampSize, 
       bx, by, bw, bh, stampWidthRatio*stampSize, stampHeightRatio*stampSize, images[i][1], images[i][2]))
   end
-  local buttonSize = bh*0.3
-  Drawer.popUp = PopUp:new(bx, by, bw, bh, bx, by + (bh - buttonSize)/2, buttonSize, buttonSize, closeImage, openImage, UIImage)
+  local _, _, buttonSize1, buttonSize2 = openImage.quads[1]:getViewport()
+  local pppw, ppph, pppx, pppy;
+  if (UIImage.type or "") == "animation" then
+    _, _, pppw, ppph = UIImage.quads[1]:getViewport()
+    pppx, pppy = bx + (pppw - bw)/2, by - (ppph - bh)/2
+  else
+    pppw, ppph = UIImage:getWidth(), UIImage:getHeight()
+    pppx, pppy = bx + (UIImage:getWidth() - bw)/2, by - (UIImage:getHeight() - bh)/2
+  end
+  print(pppw, ppph, pppx, pppy)
+  -- Drawer.popUp = PopUp:new(pppx, pppy, pppw, ppph , pppx, pppy , buttonSize, buttonSize, closeImage, openImage, UIImage)
+  Drawer.popUp = PopUp:new(bx, by, 0.66*bw, 0.66*bh, bx, by + (bh - buttonSize2)/2, 0.66*buttonSize1, 0.66*buttonSize2, closeImage, openImage, UIImage)
   Drawer.inScreen = true
   Drawer.lastActive = 0
 
 end
 
-Drawer.update = function(mx, my, dt)
-  if Drawer.inScreen then
-    Drawer.popUp:update(mx, my, dt)
-    if Drawer.popUp.UI.inScreen then
-      for i = 1, #Drawer.stamps do
-        if not Drawer.stamps[i].inScreen then
-          Drawer.stamps[i].inScreen = true
-          Drawer.stamps[i].drawUpdate = true
+Drawer.update = function(self, mx, my, dt)
+  if self.inScreen then
+    self.popUp:update(mx, my, dt)
+    if self.popUp.UI.inScreen then
+      for i = 1, #self.stamps do
+        if not self.stamps[i].inScreen then
+          self.stamps[i].inScreen = true
+          self.stamps[i].drawUpdate = true
         end
       end
     else
-      for i = 1, #Drawer.stamps do
-        Drawer.stamps[i].inScreen = false
+      for i = 1, #self.stamps do
+        self.stamps[i].inScreen = false
       end
     end
-    for i=1, #Drawer.stamps do
-      Drawer.stamps[i]:update(mx, my, dt)
-      if Drawer.stamps[i].Stamp.active then
-        for j=1,#Drawer.stamps do
-          if Drawer.stamps[i] ~= Drawer.stamps[j] then
-            Drawer.stamps[j].drawStamp = false
+    for i=1, #self.stamps do
+      self.stamps[i]:update(mx, my, dt)
+      if self.stamps[i].Stamp.active then
+        for j=1,#self.stamps do
+          if self.stamps[i] ~= self.stamps[j] then
+            self.stamps[j].drawStamp = false
           end
         end
-        Drawer.stamps[i].Stamp.active = false
-        Drawer.lastActive = i
+        self.stamps[i].Stamp.active = false
+        self.lastActive = i
       end
     end
   else
-    for i = 1, #Drawer.stamps do
-        Drawer.stamps[i].inScreen = false
-        Drawer.stamps[i].Stamp.inScreen = false
-          -- Drawer.stamps[i].drawUpdate = true
+    for i = 1, #self.stamps do
+        self.stamps[i].inScreen = false
+        self.stamps[i].Stamp.inScreen = false
+          -- self.stamps[i].drawUpdate = true
         
     end
-    Drawer.popUp.UI.inScreen = false
-    Drawer.popUp.Btt.drawUpdate = false
-    for i=1, #Drawer.stamps do
-      Drawer.stamps[i]:update(mx, my, dt)
+    self.popUp.UI.inScreen = false
+    self.popUp.Btt.drawUpdate = false
+    for i=1, #self.stamps do
+      self.stamps[i]:update(mx, my, dt)
     end
   end
 end
 
-Drawer.draw = function(inCanvas, xCanvas, yCanvas, Canvas)
-  Drawer.popUp:draw()
-  for i=1,#Drawer.stamps do
-    Drawer.stamps[i]:drawStamps(inCanvas, xCanvas, yCanvas, Canvas)
+Drawer.draw = function(self, inCanvas, xCanvas, yCanvas, Canvas)
+  self.popUp:draw()
+  for i=1,#self.stamps do
+    self.stamps[i]:drawStamps(inCanvas, xCanvas, yCanvas, Canvas)
   end
   love.graphics.setColor(1,1,1,1)
   if Canvas then love.graphics.draw(Canvas, xCanvas, yCanvas) end
-  for i=1,#Drawer.stamps do
-    Drawer.stamps[i]:drawButtons(inCanvas, xCanvas, yCanvas, Canvas)
+  for i=1,#self.stamps do
+    self.stamps[i]:drawButtons(inCanvas, xCanvas, yCanvas, Canvas)
   end
 end
 
-Drawer.mousepressed = function()
-  if Drawer.inScreen then
-    Drawer.popUp:mousepressed()
-    for i=1,#Drawer.stamps do
-      Drawer.stamps[i]:mousepressed()
+Drawer.mousepressed = function(self)
+  if self.inScreen then
+    self.popUp:mousepressed()
+    for i=1,#self.stamps do
+      self.stamps[i]:mousepressed()
     end
   end
 end
---
+++ gamescreen.lua
@@ -0,0 +1,75 @@
+local json = require "json/json"
+local GS = {}
+local Animation = require "animation"
+
+GS.spriteInfo = json.decode(love.filesystem.read("assets/animations/dims.json"))
+GS.Drawer = require "drawer"
+GS.RB = require "rulesbook"
+GS.DocImages = {
+  love.graphics.newImage("assets/driver_template.jpg"), 
+  love.graphics.newImage("assets/identity_template.jpg"), 
+  love.graphics.newImage("assets/passport_template.jpg")
+}
+
+
+GS.load = function()
+  local closedDrawer =  Animation:new(love.graphics.newImage("assets/animations/stamps_closed_spritesheet.png"), 
+  GS.spriteInfo["stamps_closed_spritesheet.png"].width, GS.spriteInfo["stamps_closed_spritesheet.png"].height, 
+  GS.spriteInfo["stamps_closed_spritesheet.png"].duration, "linear")
+
+  local openDrawer = Animation:new(love.graphics.newImage("assets/animations/stamps_spritesheet.png"), 
+  GS.spriteInfo["stamps_spritesheet.png"].width, GS.spriteInfo["stamps_spritesheet.png"].height, 
+  GS.spriteInfo["stamps_spritesheet.png"].duration, "linear")
+
+  local xSign = love.graphics.newImage("assets/x_close.png")
+
+
+  GS.Drawer.createEntities(GS.spriteInfo["stamps_spritesheet.png"].x, GS.spriteInfo["stamps_spritesheet.png"].y, 
+  GS.spriteInfo["stamps_spritesheet.png"].width, GS.spriteInfo["stamps_spritesheet.png"].height, 
+  {{love.graphics.newImage("assets/br.png"), love.graphics.newImage("assets/br.png")}, 
+  {love.graphics.newImage("assets/fi.png"), love.graphics.newImage("assets/fi.png")}, 
+  {love.graphics.newImage("assets/nl.png"), love.graphics.newImage("assets/nl.png")}}, closedDrawer, xSign, openDrawer, 1, 1
+  )
+
+
+
+  GS.backImage = Animation:new(love.graphics.newImage("assets/animations/game_bg_spritesheet.png"), 
+    GS.spriteInfo["game_bg_spritesheet.png"].width, GS.spriteInfo["game_bg_spritesheet.png"].height, 
+    1, "linear")
+
+  -- GS.backImage = Animation:new(love.graphics.newImage("game_bg_spritesheet.png"),)
+
+
+  GS.resBtt = Animation:new(love.graphics.newImage("assets/animations/reset_button_spritesheet.png"), 
+    GS.spriteInfo["reset_button_spritesheet.png"].width, GS.spriteInfo["reset_button_spritesheet.png"].height, 
+    GS.spriteInfo["reset_button_spritesheet.png"].duration, "linear")
+    -- print(love.graphics.newImage("assets/animations/game_bg_spritesheet.jpg"), 
+    -- GS.spriteInfo["game_bg_spritesheet.jpg"].width == 1920, GS.spriteInfo["game_bg_spritesheet.jpg"].height == 1080, 
+    -- GS.spriteInfo["game_bg_spritesheet.jpg"].duration == 5, "linear")
+  -- local pages = {}
+  -- GS.RB.createEntities( {} GS.spriteInfo["rulebook_spritesheet.png"], )
+end
+
+GS.update = function(dt)
+  local mx, my = love.mouse.getPosition()
+  
+  GS.backImage:update(dt)
+  GS.resBtt:update(dt)
+  GS.Drawer:update(mx, my, dt)
+end
+
+GS.draw = function()
+  -- print(GS.spriteInfo["game_bg_spritessheet.jpg"].scale)
+  GS.backImage:draw(0,0,0,GS.spriteInfo["game_bg_spritesheet.png"].scale, GS.spriteInfo["game_bg_spritesheet.png"].scale)
+
+  GS.Drawer:draw()
+
+  GS.resBtt:draw(GS.spriteInfo["reset_button_spritesheet.png"].x,GS.spriteInfo["reset_button_spritesheet.png"].y,
+  0,GS.spriteInfo["reset_button_spritesheet.png"].scale,GS.spriteInfo["reset_button_spritesheet.png"].scale)
+end
+
+GS.mousepressed = function()
+  GS.Drawer:mousepressed()
+end
+
+return GS
